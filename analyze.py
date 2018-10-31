4898a31f10cb0e02bb9f3b72177f5904fdc9bee5
beratarik <beratarik@gmail.com>
2018-10-31 15:53:29 -0400

    goes through data points in each index (hopefully correctly)

--- analyze.py
+++ analyze.py
@@ -1,10 +1,15 @@
+import time
 
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch([{'host': 'bulika.eecs.utk.edu', 'port': 9200}])
-
 for i in es.indices.get("argus-short*"):
-    print(i)
-    result = es.search(index=i)
-
-print(result)
+    res = es.search(index=i, scroll = '2m', size = 1000)
+    sid = res['_scroll_id']
+    scroll_size = res['hits']['total']
+    
+    while (scroll_size > 0):
+        res = es.scroll(scroll_id = sid, scroll = '2m')
+        sid = res['_scroll_id']
+        scroll_size = len(res['hits']['hits']) 
+        print(res)
