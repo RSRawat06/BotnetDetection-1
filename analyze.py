65b1dec22fca27f9fb1b2d7b807a854d68f3d97c
Toy Reid <toymreid@gmail.com>
2018-10-31 16:03:55 -0400

    It works yay

--- analyze.py
+++ analyze.py
@@ -4,12 +4,12 @@ import json
 
 es = Elasticsearch([{'host': 'bulika.eecs.utk.edu', 'port': 9200}])
 for i in es.indices.get("argus-short*"):
-    res = es.search(index=i, scroll = '2m', size = 1000)
-    sid = res['_scroll_id']
-    scroll_size = res['hits']['total']
+	res = es.search(index=i, scroll = '2m', size = 1000)
+	sid = res['_scroll_id']
+	scroll_size = res['hits']['total']
     
-    while (scroll_size > 0):
-        res = es.scroll(scroll_id = sid, scroll = '2m')
-        sid = res['_scroll_id']
-        scroll_size = len(res['hits']['hits']) 
-        print(res)
+	while (scroll_size > 0):
+		res = es.scroll(scroll_id = sid, scroll = '2m')
+		sid = res['_scroll_id']
+		scroll_size = len(res['hits']['hits']) 
+		print(json.dumps(res))
