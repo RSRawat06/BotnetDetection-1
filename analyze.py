c9a7b1eaff523f2e6af7b5be792b055b0ff9fefa
Toy Reid <toymreid@gmail.com>
2018-10-31 15:53:47 -0400

    stuff

--- analyze.py
+++ analyze.py
@@ -1,8 +1,10 @@
 from elasticsearch import Elasticsearch
+import json
 
 es = Elasticsearch([{'host': 'bulika.eecs.utk.edu', 'port': 9200}])
 
 for i in es.indices.get("argus-short*"):
-	print(i)
-	result = es.search(index=i)
+	# print(i)
+	result = es.search(index=i, size=10000)
+	data = json.loads(result)
 	print(result)
