2932698909c9362ca256d97c3696abf1308451e9
Toy Reid <toymreid@gmail.com>
2018-10-31 15:05:33 -0400

    Moved print

--- analyze.py
+++ analyze.py
@@ -1,10 +1,8 @@
-
 from elasticsearch import Elasticsearch
 
 es = Elasticsearch([{'host': 'bulika.eecs.utk.edu', 'port': 9200}])
 
 for i in es.indices.get("argus-short*"):
-    print(i)
-    result = es.search(index=i)
-
-print(result)
+	print(i)
+	result = es.search(index=i)
+	print(result)
