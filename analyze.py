d2dc3fa7facadede842a044ee073a28103a1c2bf
Toy Reid <toymreid@gmail.com>
2018-11-09 13:22:02 -0500

    Merging

--- analyze.py
+++ analyze.py
@@ -4,12 +4,13 @@ import json
 
 es = Elasticsearch([{'host': 'bulika.eecs.utk.edu', 'port': 9200}])
 for i in es.indices.get("argus-short*"):
-	res = es.search(index=i, scroll = '2m', size = 1000)
+	res = es.search(index=i, scroll='2m', size=1000)
 	sid = res['_scroll_id']
 	scroll_size = res['hits']['total']
     
 	while (scroll_size > 0):
-		res = es.scroll(scroll_id = sid, scroll = '2m')
+		res = es.scroll(scroll_id=sid, scroll='2m')
 		sid = res['_scroll_id']
 		scroll_size = len(res['hits']['hits']) 
 		print(json.dumps(res))
+		
