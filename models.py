e4060afb4c2f91a2fb748843082455ed6d3eb787
Wyrine <kashahat@gmail.com>
2018-12-02 15:19:11 -0500

    added random forests

--- models.py
+++ models.py
@@ -2,9 +2,16 @@ import numpy as np
 import pandas as pd
 from sklearn.neural_network import MLPClassifier as mlp
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier as rf
 
 def bpnn(X, Y, epochs = 20):
     model = mlp(max_iter=epochs, verbose=True)
+    X_tr, X_te, Y_tr, Y_te = train_test_split(X, Y)
+    model.fit(X_tr, Y_tr)
+    print(model.score(X_te, Y_te))
+
+def randForest(X, Y, n = 20, c = "entropy"):
+    model = rf(verbose=1, criterion = c, n_estimators = n)
     X_tr, X_te, Y_tr, Y_te = train_test_split(X, Y)
     model.fit(X_tr, Y_tr)
     print(model.score(X_te, Y_te))
