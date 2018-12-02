0c88d8d44af02c9326a464fcf510ffa215cb7f20
Wyrine <kashahat@gmail.com>
2018-12-02 15:01:19 -0500

    bpnn'd

--- bpnn.py
+++ bpnn.py
@@ -3,12 +3,8 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier as mlp
 from sklearn.model_selection import train_test_split
 
-def bpnn(data, file = False, epochs = 20):
-    model = mlp(max_iter=epochs)
-    if file != False:
-        data = pandas.read_csv(file)
-    #do one-hot encoding
-    X_tr, X_te, Y_tr, Y_te = train_test_split(data)
-
+def bpnn(X, Y, epochs = 20):
+    model = mlp(max_iter=epochs, verbose=True)
+    X_tr, X_te, Y_tr, Y_te = train_test_split(X, Y)
     model.fit(X_tr, Y_tr)
-    #model.
+    print(model.score(X_te, Y_te))
