31c0917757f224dab59c6215f69b2445049da9d4
Toy Reid <toymreid@gmail.com>
2018-12-07 20:04:41 -0500

    writing to csv

--- analyze.py
+++ analyze.py
@@ -3,6 +3,8 @@ import json
 from elasticsearch import Elasticsearch
 from bs4 import BeautifulSoup
 from netaddr import IPNetwork
+import pandas as pd
+import json
 import sys
 
 class IPFinder:
@@ -17,42 +19,71 @@ class IPFinder:
             else:
                 self.firstIP.append(line)
                 self.lastIP.append(str(IPNetwork(line)[-1]))
+        
+        self.botnetCount = 1
+        self.normalCount = 1
+        self.reachCount = 1000
+        self.netList = []
 
     def feedData(self):
         es = Elasticsearch([{'host': 'bulika.eecs.utk.edu', 'port': 9200}])
-        for i in es.indices.get("argus-live-*"):
-            res = es.search(index=i, scroll = '2m', size = 1)
-            sid = res['_scroll_id']
-            scroll_size = res['hits']['total']
+        with open('out.csv', 'w+') as outFile:
+            for i in es.indices.get("argus-live-*"):
+                if self.botnetCount > 1000 and self.normalCount > 1000:
+                    print("Found {0} botnets and {1} normals".format(self.botnetCount, self.normalCount))
+                    self.createCSV()
+                    sys.exit(1)
+                
+                res = es.search(index=i, scroll = '2m', size = 1)
+                sid = res['_scroll_id']
+                scroll_size = res['hits']['total']
+                
+                count = 0
+                print("Data for %s" % (i))
+                while (scroll_size > 0):
+                    count += 1
+                    res = es.scroll(scroll_id = sid, scroll = '2m')
+
+                    if self.botnetCount > 1000 and self.normalCount > 1000:
+                        print("Found {0} botnets and {1} normals".format(self.botnetCount, self.normalCount))
+                        self.createCSV()
+                        sys.exit(1)
+                    elif res['hits']['hits'][0]['_source']['doc']['dst']['security'] != ['B']\
+                        and res['hits']['hits'][0]['_source']['doc']['src']['security'] != ['B']\
+                        and self.normalCount <= 1000:
+                        print(" Found normal #{0}".format(self.normalCount))
+                        self.normalCount += 1
+                        res['security'] = 0
+                        self.netList.append(res)
+
+                    sid = res['_scroll_id']
+                    scroll_size = len(res['hits']['hits'])
+                    #print(json.dumps(res, indent = 4))
+                    #print(json.dumps(res['hits']['hits'][0], indent = 4))
+                    #print("IP is %d" % (res['hits']['hits'][0]['_source']['doc']))
+                    s = res['hits']['hits'][0]['_source']['doc']
+                    dst = s['dst']['ip']
+                    src = s['src']['ip']
             
-            count = 0
-            print("Data for %s" % (i))
-            while (scroll_size > 0):
-            	count += 1
-            	res = es.scroll(scroll_id = sid, scroll = '2m')
-            	sid = res['_scroll_id']
-            	scroll_size = len(res['hits']['hits'])
-            	#print(json.dumps(res, indent = 4))
-            	#print(json.dumps(res['hits']['hits'][0], indent = 4))
-            	#print("IP is %d" % (res['hits']['hits'][0]['_source']['doc']))
-            	s = res['hits']['hits'][0]['_source']['doc']
-            	dst = s['dst']['ip']
-            	src = s['src']['ip']
-        
-            	try:
-                    # print("Destination IP is %s " % (dst));
-                    # print("Source IP is %s " % (src));
-                    # TODO: Handle IPv6
-                    if dst.find(":") == -1:
-                        self.findIP('d', dst, src)
-                    elif src.find(":") == -1:
-                        self.findIP('s', src, dst)
-                    
-            	except IndexError:
-                    break
-        
-            print("Data for %s" % (i))
-            print("%d connections read" % (count))
+                    try:
+                        # print("Destination IP is %s " % (dst));
+                        # print("Source IP is %s " % (src));
+                        # TODO: Handle IPv6
+                        if dst.find(":") == -1:
+                            findDst = self.findIP('d', dst, src)
+                        if src.find(":") == -1:
+                            findSrc = self.findIP('s', src, dst)
+                        if findDst and findSrc and self.botnetCount <= 1000:
+                            print("\tFound botnet #{0}".format(self.botnetCount))
+                            self.botnetCount += 1
+                            res['security'] = 1
+                            self.netList.append(res)
+                        
+                    except IndexError:
+                        break
+            
+                print("Data for %s" % (i))
+                print("%d connections read" % (count))
         
     # TODO: Distinguish between source/destination IPs (print differently)
     def findIP(self, direction, ip, connectionip):
@@ -66,15 +97,88 @@ class IPFinder:
                 if ip[1] >= firstip[1] and ip[1] <= lastip[1]:
                     if ip[2] >= firstip[2] and ip[2] <= lastip[2]:
                         if ip[3] >= firstip[3] and ip[3] <= lastip[3]:
-							if direction == 'd':
-                                print("%s connecting with malicious IP %s" % (ip, connectionip))
-                                #print()
-                            else:
-                                print("malicious IP %s connecting with %s" % (ip, connectionip))                    
-                                #print()
-                            #print("{0} detected as a malicious IP".format(ip)
+                            # if direction == 'd':
+                            #     print("%s connecting with malicious IP %s" % (ip, connectionip))
+                            # else:
+                            #     print("malicious IP %s connecting with %s" % (ip, connectionip))   
+                            return True
+
+                            
             if firstip[0] > ip[0]:
             	break
+        
+        return False
+
+    def createCSV(self):
+        print("Writing to CSV")
+        with open('out.csv', 'w+') as outFile:
+            outFile.write(
+                "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23},{24},{24},{25}\n"\
+                .format(
+                    'appbytes',
+                    'bytes',
+                    'count',
+                    'dstbytes',
+                    'dstload',
+                    'dstport',
+                    'dstrate',
+                    'duration',
+                    'hops',
+                    'intpkt',
+                    'jitter',
+                    'loss',
+                    'maxsize',
+                    'meansize',
+                    'minsize',
+                    'packets',
+                    'pcr',
+                    'ploss',
+                    'protocol',
+                    'rate',
+                    'rtt',
+                    'srcbytes',
+                    'srcload',
+                    'srcport',
+                    'srcrate',
+                    'security'
+                )
+            )
+            print("Size =", len(self.netList))
+            for r in self.netList:
+                doc = r['hits']['hits'][0]['_source']['doc']
+                outFile.write(
+                    "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10},{11},{12},{13},{14},{15},{16},{17},{18},{19},{20},{21},{22},{23},{24},{24},{25}\n"\
+                    .format(
+                        doc['appbytes'],
+                        doc['bytes'],
+                        doc['count'],
+                        doc['dst']['bytes'],
+                        doc['dst']['load'],
+                        doc['dst']['port'],
+                        doc['dst']['rate'],
+                        doc['duration'],
+                        doc['hops'],
+                        doc['intpkt'],
+                        doc['jitter'],
+                        doc['loss'],
+                        doc['maxsize'],
+                        doc['meansize'],
+                        doc['minsize'],
+                        doc['packets'],
+                        doc['pcr'],
+                        doc['ploss'],
+                        doc['protocol'],
+                        doc['rate'],
+                        doc['rtt'],
+                        doc['src']['bytes'],
+                        doc['src']['load'],
+                        doc['src']['port'],
+                        doc['src']['rate'],
+                        r['security']
+                    )
+                )
+
+        return 0
             
 def main():
     I = IPFinder()
